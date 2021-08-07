288215a5ecf61bbca5b4ae81a8bebfa9c8af079b
Bartko <rbartk01@atkearney.com>
2021-08-07 20:42:20 +0200

    Added Cookies service; Added basic http responses status validation for Blockchain component

--- DTFrontEnd/src/app/app.module.ts
+++ DTFrontEnd/src/app/app.module.ts
@@ -5,22 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CookieModule, CookieService } from 'ngx-cookie';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BinanceComponent } from './binance/binance.component';
 import { KucoinComponent } from './kucoin/kucoin.component';
 import { BlockchainComponent } from './blockchain/blockchain.component';
 
 @NgModule({
-  declarations: [AppComponent, BinanceComponent, KucoinComponent, BlockchainComponent], // komponenty, dyrektywy i konweretry (pipes) które wchodzą w skłdd tego modułu
+  declarations: [
+    AppComponent,
+    BinanceComponent,
+    KucoinComponent,
+    BlockchainComponent,
+  ], // komponenty, dyrektywy i konweretry (pipes) które wchodzą w skłdd tego modułu
   imports: [
     BrowserModule,
     AppRoutingModule,
     CommonModule,
     HttpClientModule,
     FormsModule,
+    CookieModule.forRoot(),
     //BrowserAnimationsModule,
   ], // inne moduły wykorzystywane w tym module
   providers: [
+    CookieService,
     //{ provide: HTTP_INTERCEPTORS, useClass: null, multi: true },
   ], // serwisy z tego modułu, które będą dostepne z dowolnego miejsca w ekosystemie apki
   bootstrap: [AppComponent], // główne komponenty apki
--- DTFrontEnd/src/app/blockchain/blockchain.component.ts
+++ DTFrontEnd/src/app/blockchain/blockchain.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { CookieService } from 'ngx-cookie';
 import { BlockchainService } from '../services/blockchain.service';
 
 @Component({
   selector: 'app-blockchain',
   templateUrl: './blockchain.component.html',
-  styleUrls: ['./blockchain.component.css']
+  styleUrls: ['./blockchain.component.css'],
 })
 export class BlockchainComponent implements OnInit {
   currentBalance: number = 0;
@@ -12,24 +14,69 @@ export class BlockchainComponent implements OnInit {
   selectedAddress: string = '1P5ZEDWTKTFGxQjZphgWPQUpe554WKDfHQ';
   btcDivider: number = 100000000;
 
-  constructor(private BlockchainService: BlockchainService) { }
+  statusOk = ['200'];
+  statusNotOk = ['404'];
+  pauseRequired = 5 * 60 * 1000; // 5 min required pause after statusNotOk <- trzeba to wziąć z info o API
 
-  ngOnInit(): void {
-  }
+  statusKey = `${this.element.nativeElement.tagName}-Status`;
+  timeStampKey = `${this.element.nativeElement.tagName}-TimeStamp`;
+
+  constructor(
+    private BlockchainService: BlockchainService,
+    private cookie: CookieService,
+    private element: ElementRef
+  ) {}
+
+  ngOnInit(): void {}
 
   getCurrentAccountBalance(account: string) {
-    this.BlockchainService.getCurrentAccountData(account).subscribe((response) => {
-      this.currentBalance = response.final_balance / this.btcDivider;
-    })
+    if (this.StatusValidator()) {
+      this.BlockchainService.getCurrentAccountData(account).subscribe(
+        (response) => {
+          this.updateCooke(response);
+          var body = response.body;
+          this.currentBalance = body.final_balance / this.btcDivider;
+        }
+      );
+    }
   }
 
   getCurrentLastTransactions(account: string) {
-    this.BlockchainService.getCurrentAccountData(account).subscribe((response) => {
-      this.lastTransactions = response.value;
-      this.lastTransactions.forEach(element => {
-        element /= this.btcDivider;
-      });
-    })
+    this.BlockchainService.getCurrentAccountData(account).subscribe(
+      (response) => {
+        var body = response.body;
+        this.lastTransactions = body.value;
+        this.lastTransactions.forEach((element) => {
+          element /= this.btcDivider;
+        });
+      }
+    );
   }
 
+  StatusValidator(): boolean {
+    var isFirstTime = !this.cookie.hasKey(this.statusKey);
+    var isStatusOk = this.statusOk.includes(this.cookie.get(this.statusKey));
+    var currentTimeStamp = Date.now();
+    var isPauseEnough =
+      currentTimeStamp - Number(this.cookie.get(this.timeStampKey)) >=
+      this.pauseRequired;
+    var isNotOkAfterRequiredPause =
+      this.statusNotOk.includes(this.cookie.get(this.statusKey)) &&
+      isPauseEnough;
+
+    if (isFirstTime) {
+      console.log('First Time cookie called...');
+      return true;
+    } else if (isStatusOk || isNotOkAfterRequiredPause) {
+      console.log('Status is OK or Status Not OK but After Pause...');
+      return true;
+    }
+    return false;
+  }
+
+  updateCooke(response: HttpResponse<any>) {
+    var timeStamp = Date.now();
+    this.cookie.put(this.statusKey, response.status.toString());
+    this.cookie.put(this.timeStampKey, timeStamp.toString());
+  }
 }
