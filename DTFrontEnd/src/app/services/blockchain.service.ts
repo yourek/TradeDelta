1359f40e17f970f04565ba942bcafa782761a647
Pawel <pawelwojtowicz89@gmail.com>
2021-08-08 22:39:21 +0200

    Revert "to merge, old stuff"
    This reverts commit b199be26de7ccc6d3fd59f1a77b137abf95d4877.

--- DTFrontEnd/src/app/services/blockchain.service.ts
+++ DTFrontEnd/src/app/services/blockchain.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { BaseRouteReuseStrategy } from '@angular/router';
 
-//const BASE_PATH = "https://api.blockchain.com/v3/exchange/";
+//const BASE_PATH = "https://api.blockchain.com/v3/exchange/".replace(/\/+$/, "");
 const BASE_PATH = "https://blockchain.info/rawaddr/";
 
 @Injectable({
@@ -12,15 +12,7 @@ export class BlockchainService {
 
   constructor(private http: HttpClient) { }
 
-  // getCurrentAccountDataOld(account: string, limit: number) {
-  //   return this.http.get<any>(BASE_PATH + account, {
-  //     params: { limit: limit },
-  //   });
-  // }
-
-  getCurrentAccountData(account: string, limit: string) {
-    return this.http.get<any>(BASE_PATH + account, {
-      params: { limit: limit },
-    });
+  getCurrentAccountData(account: string) {
+    return this.http.get<any>(BASE_PATH + account);
   }
 }
